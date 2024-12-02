bbfd35be92ff4bbdf29ed54b0bc9f1d934cdd715
Corey Farwell <coreyf@rwell.org>
2024-12-02 09:45:30 -0500

    format code

--- run.py
+++ run.py
@@ -18,18 +18,19 @@ from typing import Dict, Generator, NamedTuple, Set
 GEOHASH_PRECISION = 5
 NUM_CLUSTERS = 5
 COLORS = [
-    "#"+''.join([random.choice('0123456789ABCDEF')
-    for _ in range(6)])
+    "#" + "".join([random.choice("0123456789ABCDEF") for _ in range(6)])
     for _ in range(1000)
 ]
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename="example.log", encoding="utf-8", level=logging.DEBUG)
+
 
 class Row(NamedTuple):
     lat: float
     lon: float
     taxon_id: int
+
 
 def read_float(value: str) -> float:
     try:
@@ -37,40 +38,45 @@ def read_float(value: str) -> float:
     except ValueError:
         return None
 
+
 def read_int(value: str) -> int:
     try:
         return int(value)
     except ValueError:
         return None
 
+
 def read_rows() -> Generator[Row, None, None]:
-    with open('occurrence.txt', 'r') as f:
-        reader = csv.DictReader(f, delimiter='\t')
+    with open("occurrence.txt", "r") as f:
+        reader = csv.DictReader(f, delimiter="\t")
         for row in reader:
-            lat, lon = read_float(row['decimalLatitude']), read_float(row['decimalLongitude'])
+            lat, lon = read_float(row["decimalLatitude"]), read_float(
+                row["decimalLongitude"]
+            )
             if not (lat and lon):
-                logger.error(f"Invalid latitude or longitude: {row['decimalLatitude']}, {row['decimalLongitude']}")
+                logger.error(
+                    f"Invalid latitude or longitude: {row['decimalLatitude']}, {row['decimalLongitude']}"
+                )
                 continue
-            taxon_id = read_int(row['taxonID'])
+            taxon_id = read_int(row["taxonID"])
             if not taxon_id:
                 logger.error(f"Invalid taxon ID: {row['taxonID']}")
                 continue
             yield Row(lat, lon, taxon_id)
 
+
 class Point(NamedTuple):
     lat: float
     lon: float
 
+
 # bbox lower left, bbox upper right
 def geohash_to_rect(geohash):
     lat, lon, lat_err, lon_err = pygeohash.decode_exactly(geohash)
-    return Point(
-        lat=lat - lat_err,
-        lon=lon - lon_err
-    ), Point(
-        lat=lat + lat_err,
-        lon=lon + lon_err
+    return Point(lat=lat - lat_err, lon=lon - lon_err), Point(
+        lat=lat + lat_err, lon=lon + lon_err
     )
+
 
 def build_geojson_feature(geohash: str, cluster: int):
     sw_bbox, ne_bbox = geohash_to_rect(geohash)
@@ -89,8 +95,9 @@ def build_geojson_feature(geohash: str, cluster: int):
             "stroke-width": 0,
             "cluster": int(cluster),
         },
-        "geometry": {"type": "Polygon", "coordinates": [coords]}
+        "geometry": {"type": "Polygon", "coordinates": [coords]},
     }
+
 
 def build_condensed_distance_matrix():
     geohash_to_taxon_id_to_count: Dict[str, Dict[int, int]] = {}
@@ -100,8 +107,12 @@ def build_condensed_distance_matrix():
 
     for row in read_rows():
         geohash = pygeohash.encode(row.lat, row.lon, precision=GEOHASH_PRECISION)
-        geohash_to_taxon_id_to_count[geohash] = geohash_to_taxon_id_to_count.get(geohash, {})
-        geohash_to_taxon_id_to_count[geohash][row.taxon_id] = geohash_to_taxon_id_to_count[geohash].get(row.taxon_id, 0) + 1
+        geohash_to_taxon_id_to_count[geohash] = geohash_to_taxon_id_to_count.get(
+            geohash, {}
+        )
+        geohash_to_taxon_id_to_count[geohash][row.taxon_id] = (
+            geohash_to_taxon_id_to_count[geohash].get(row.taxon_id, 0) + 1
+        )
         seen_taxon_id.add(row.taxon_id)
 
     ordered_seen_taxon_id = sorted(seen_taxon_id)
@@ -116,25 +127,26 @@ def build_condensed_distance_matrix():
 
     whitened = whiten(matrix)
 
-    return ordered_seen_geohash, pdist(whitened, metric='braycurtis')
+    return ordered_seen_geohash, pdist(whitened, metric="braycurtis")
 
-if os.path.exists('condensed_distance_matrix.pickle'):
+
+if os.path.exists("condensed_distance_matrix.pickle"):
     logger.info("Loading condensed distance matrix")
-    with open('condensed_distance_matrix.pickle', 'rb') as f:
+    with open("condensed_distance_matrix.pickle", "rb") as f:
         ordered_seen_geohash, condensed_distance_matrix = pickle.load(f)
 else:
     ordered_seen_geohash, condensed_distance_matrix = build_condensed_distance_matrix()
     logger.info("Saving condensed distance matrix")
-    with open('condensed_distance_matrix.pickle', 'wb') as f:
+    with open("condensed_distance_matrix.pickle", "wb") as f:
         pickle.dump((ordered_seen_geohash, condensed_distance_matrix), f)
 
 # Generate the linkage matrix
-Z = linkage(condensed_distance_matrix, 'ward')
+Z = linkage(condensed_distance_matrix, "ward")
 # fig = plt.figure(figsize=(25, 10))
 # dn = dendrogram(Z, labels=ordered_seen_geohash)
 # plt.show()
 
-clusters = fcluster(Z, t=3, criterion='distance')
+clusters = fcluster(Z, t=3, criterion="distance")
 logger.info(f"Number of clusters: {len(set(clusters))}")
 
 feature_collection = {
@@ -142,8 +154,7 @@ feature_collection = {
     "features": [
         build_geojson_feature(geohash, cluster)
         for geohash, cluster in zip(ordered_seen_geohash, clusters)
-    ]
+    ],
 }
-with open('clusters.geojson', 'w') as f:
+with open("clusters.geojson", "w") as f:
     json.dump(feature_collection, f)
-
