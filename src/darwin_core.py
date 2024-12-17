0374dcf30754b0ccd9f14d128ac52c9af4598048
Corey Farwell <coreyf@rwell.org>
2024-12-17 13:50:48 -0500

    speed up logging

--- src/darwin_core.py
+++ src/darwin_core.py
@@ -1,20 +1,30 @@
-import csv
-from typing import Dict, Generator, NamedTuple, Optional, Self
+from typing import Generator
 import logging
 import polars as pl
-from src.point import Point
 from contexttimer import Timer
+import os
 
 logger = logging.getLogger(__name__)
 
 type TaxonId = int
 
 
-def read_rows(input_file: str) -> Generator[pl.DataFrame, None, None]:
-    with open(input_file, "rb") as f:
-        num_lines = sum(1 for _ in f)
+def read_rows(
+    input_file: str, n_batches: int = 5
+) -> Generator[pl.DataFrame, None, None]:
+    # Get file size
+    file_size = os.path.getsize(input_file)
 
-    logger.info(f"Processing CSV file ({num_lines} lines)")
+    # Skipping the header, read first two five lines to estimate average line length
+    with open(input_file, "rb") as f:
+        # Skip header
+        f.readline()
+        avg_line_size = sum(len(f.readline()) for _ in range(5)) / 5
+
+    # Estimate total lines (subtract 1 for header)
+    num_lines = (file_size // avg_line_size) - 1
+
+    logger.info(f"Processing CSV file (estimated {num_lines:,} lines)")
 
     reader = pl.read_csv_batched(
         input_file,
@@ -38,14 +48,21 @@ def read_rows(input_file: str) -> Generator[pl.DataFrame, None, None]:
             "recordedBy",
         ],
     )
-    batches = reader.next_batches(n=5)
+    batches = reader.next_batches(n=n_batches)
     processed_lines = 0
+    last_percentage = -1  # Track the last logged percentage
     while batches:
         for batch in batches:
-            with Timer(
-                output=logger.info,
-                prefix=f"Processing CSV file: {processed_lines}/{num_lines} lines ({(processed_lines/num_lines)*100:.1f}%)",
-            ):
-                yield batch
+            current_percentage = int((processed_lines / num_lines) * 100)
             processed_lines += len(batch)
-        batches = reader.next_batches(n=5)
+            # Only log if percentage has increased by at least 1%
+            if current_percentage > last_percentage:
+                with Timer(
+                    output=logger.info,
+                    prefix=f"Processed CSV file: {processed_lines} lines (~{current_percentage}%)",
+                ):
+                    yield batch
+                last_percentage = current_percentage
+            else:
+                yield batch
+        batches = reader.next_batches(n=n_batches)
