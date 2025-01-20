2decddef676007428b452e2bbafb91c4825288a3
Corey Farwell <coreyf@rwell.org>
2025-01-20 10:49:30 -0500

    utilize new module

--- run.py
+++ run.py
@@ -10,11 +10,11 @@ import polars as pl
 from sklearn.decomposition import IncrementalPCA
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 from contexttimer import Timer
-from scipy.cluster.hierarchy import linkage, fcluster, dendrogram
+from scipy.cluster.hierarchy import linkage, fcluster
 from scipy.spatial.distance import pdist
 from typing import Callable, List
 import typer
-from src import cluster_index, geohash
+from src import cluster_index, geohash, dendrogram, cli_output
 from src.cluster_color_builder import ClusterColorBuilder
 from src.cluster_stats import Stats
 from src.darwin_core_aggregations import DarwinCoreAggregations
@@ -123,108 +123,11 @@ def build_condensed_distance_matrix(
     return Y
 
 
-def print_cluster_stats(
-    cluster: ClusterId,
-    geohashes: List[geohash.Geohash],
-    darwin_core_aggregations: DarwinCoreAggregations,
-    all_stats: Stats,
-) -> None:
-    stats = Stats.build(darwin_core_aggregations, geohash_filter=geohashes)
-    print("-" * 10)
-    print(f"cluster {cluster} (count: {len(geohashes)})")
-
-    for kingdom, species, count in (
-        stats.taxon.sort(by="count", descending=True)
-        .limit(5)
-        .select(["kingdom", "species", "count"])
-        .collect()
-        .iter_rows(named=False)
-    ):
-        average = (
-            stats.taxon.filter(
-                pl.col("kingdom") == kingdom, pl.col("species") == species
-            )
-            .collect()
-            .get_column("average")
-            .item()
-        )
-        all_average = (
-            all_stats.taxon.filter(
-                pl.col("kingdom") == kingdom, pl.col("species") == species
-            )
-            .collect()
-            .get_column("average")
-            .item()
-        )
-
-        # If the difference between the average of the cluster and the average of all is greater than 20%, print it
-        percent_diff = (average / all_average * 100) - 100
-        if abs(percent_diff) > 20:
-            # Print the percentage difference
-            print(f"{species} ({kingdom}):")
-            print(
-                f"  - Percentage difference: {'+' if percent_diff > 0 else ''}{percent_diff:.2f}%"
-            )
-            print(f"  - Proportion: {average * 100:.2f}%")
-            print(f"  - Count: {count}")
-
-
-def print_all_cluster_stats(
-    darwin_core_aggregations: DarwinCoreAggregations, all_stats: Stats
-) -> None:
-    for kingdom, species, count in (
-        all_stats.taxon.sort(by="count", descending=True)
-        .limit(5)
-        .select(["kingdom", "species", "count"])
-        .collect()
-        .iter_rows(named=False)
-    ):
-        average = (
-            all_stats.taxon.filter(
-                pl.col("kingdom") == kingdom, pl.col("species") == species
-            )
-            .collect()
-            .get_column("average")
-            .item()
-        )
-        print(f"{species} ({kingdom}):")
-        print(f"  - Proportion: {average * 100:.2f}%")
-        print(f"  - Count: {count}")
-
-
-def show_dendrogram(Z: np.ndarray, ordered_seen_geohash: List[geohash.Geohash]) -> None:
-    plt.figure()
-    dendrogram(
-        Z,
-        labels=ordered_seen_geohash,
-        leaf_label_func=lambda id: (
-            ordered_seen_geohash[id]
-            if geohash.is_water(ordered_seen_geohash[id])
-            else ""
-        ),
-    )
-    plt.show()
-
-
 def write_geojson(
     feature_collection: geojson.FeatureCollection, output_file: str
 ) -> None:
     with open(output_file, "w") as geojson_writer:
         geojson.dump(feature_collection, geojson_writer)
-
-
-def print_results(
-    darwin_core_aggregations: DarwinCoreAggregations,
-    all_stats: Stats,
-    clusters: cluster_index.ClusterIndex,
-) -> None:
-    # For each top count taxon, print the average per geohash
-    print_all_cluster_stats(darwin_core_aggregations, all_stats)
-
-    logger.info(f"Number of clusters: {cluster_index.num_clusters(clusters)}")
-
-    for cluster, geohashes in cluster_index.iter_clusters_and_geohashes(clusters):
-        print_cluster_stats(cluster, geohashes, darwin_core_aggregations, all_stats)
 
 
 def cluster(
@@ -240,7 +143,7 @@ def cluster(
     clusters = list(map(int, fcluster(Z, t=num_clusters, criterion="maxclust")))
 
     if show_dendrogram_opt:
-        show_dendrogram(Z, ordered_seen_geohash)
+        dendrogram.show(Z, ordered_seen_geohash)
 
     return cluster_index.build(ordered_seen_geohash, clusters)
 
@@ -284,7 +187,7 @@ def run(
         )
     )
 
-    print_results(darwin_core_aggregations, all_stats, cluster_dataframe)
+    cli_output.print_results(darwin_core_aggregations, all_stats, cluster_dataframe)
 
     if plot:
         plot_clusters(feature_collection)
