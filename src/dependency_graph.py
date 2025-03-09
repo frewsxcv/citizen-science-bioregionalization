f948e7402d1038e82c1b8ad9beb8b9eb9d6f7c39
Corey Farwell <coreyf@rwell.org>
2025-03-08 22:34:16 -0500

    mermaid improvements

--- src/dependency_graph.py
+++ src/dependency_graph.py
@@ -3,20 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 from src.data_container import DataContainer
-from src.dataframes import (
-    cluster_color,
-    cluster_neighbors,
-    cluster_taxa_statistics,
-    geocode_boundary,
-    geocode_cluster,
-    geocode_taxa_counts,
-    taxonomy,
-    taxa_geographic_mean,
-    cluster_significant_differences,
-    geocode,
-)
-from src.lazyframes import darwin_core_csv
-from src.matrices import distance, connectivity
+from run import *
 
 
 def build_dependency_graph():
@@ -37,10 +24,10 @@ def plot_dependency_graph():
     G = build_dependency_graph()
 
     plt.figure(figsize=(12, 8))
-    pos = nx.spring_layout(G)
+    pos = nx.shell_layout(G)
 
-    nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=2000)
-    nx.draw_networkx_edges(G, pos, arrowstyle='->', arrowsize=20, edge_color='gray')
+    nx.draw_networkx_nodes(G, pos, node_color='lightblue')
+    nx.draw_networkx_edges(G, pos, edge_color='gray')
     nx.draw_networkx_labels(G, pos, font_size=10, font_weight='bold')
 
     plt.title("DataContainer Dependency Graph")
